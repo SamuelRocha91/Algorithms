17c861098aa58c4e01a83e2caec78432acb7e4a1
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-08-24 18:04:31 -0300

    requisito 02 - testes

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(2, 0)
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("xablau", "tiririca")
+
+    assert encrypt_message("xablau", 8) == "ualbax"
+
+    assert encrypt_message("xablau", 3) == "bax_ual"
+
+    assert encrypt_message("xablau", 4) == "ua_lbax"
