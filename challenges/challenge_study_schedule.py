12486c77d981952a642cedaf53aa3bf021abe57b
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-08-24 14:57:32 -0300

    requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time or type(target_time) != int:
+        return None
+    count_frequency = 0
+    for entry, exit in permanence_period:
+        if type(entry) != int or type(exit) != int:
+            return None
+        if entry <= target_time <= exit:
+            count_frequency += 1
+
+    return count_frequency
