31e253000053df585db493bd6fd0e2e458f0610f
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-10-17 21:42:22 -0300

    implementa funcao que encontra elemento duplicado

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,12 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or len(nums) == 0:
+        return False
+    non_repeated = set()
+    for num in nums:
+        if type(num) is str or num < 0:
+            return False
+        if num not in non_repeated:
+            non_repeated.add(num)
+        else:
+            return num
+    return False
